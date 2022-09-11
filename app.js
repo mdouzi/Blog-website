315eeb5e98a91f68770c0ae6501ce96f9d152196
Med douzi <78613047+meddouzi@users.noreply.github.com>
2022-09-11 15:46:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -65,6 +65,8 @@ app.get("/posts/:postName", function(req, res){
 
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+app.listen(process.env.PORT || 3000, function () {  
+    console.log("serv is on")
+})
+
+
